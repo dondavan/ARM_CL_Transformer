9d5bf1591d50411cca0297a8e9a360db06baf025
dondavan <yeezushugh@gmail.com>
2024-05-08 14:53:39 +0200

    1-12

--- build_to_khadas.sh
+++ build_to_khadas.sh
@@ -1,4 +1,4 @@
 git pull
 scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 examples=1 os=linux arch=armv8a -j4
-scp build/libarm_compute* Khadas.local:/home/run/lib 
-scp build/examples/graph_vanilla_transformer Khadas.local:/home/run 
+scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
+scp build/examples/graph_vanilla_transformer Khadas.local:/home/khadas/run
