78da5f68a2ec17b10ff1f375da0fb3f9a4622567
dondavan <yeezushugh@gmail.com>
2024-05-13 10:20:50 +0200

    1-12

--- cp.sh
+++ cp.sh
@@ -1,5 +1,5 @@
 git pull
-scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 examples=1 os=linux arch=armv8a -j4
+scons Werror=1 debug=0 asserts=1 neon=1 opencl=1 examples=1 os=linux arch=armv8a -j4
 cp build/libarm_compute* /home/run/lib 
 cp build/examples/graph_bert_base_uncased /home/run 
 cp build/examples/graph_bert_large_uncased /home/run
