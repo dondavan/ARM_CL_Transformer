53e6aa1b649ef3170befe408efdacb6e614599bb
dondavan <yeezushugh@gmail.com>
2024-05-13 09:35:15 +0200

    1-12

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -131,11 +131,7 @@ class GraphVanillaTransformerExample : public Example
             add_encoder_block(data_path,"layer_11/" /*Layer Parameter Dir*/, d_model, h, eps, d_ff);
 
         // Pooler
-        graph << LinearLayer(LinearLayerInfo(d_model, TensorShape(d_ff, d_model) ,
-                                               TensorShape(d_model) ),
-                               get_weights_accessor(data_path, "pooler_weight.npy"),
-                               get_weights_accessor(data_path, "pooler_bias.npy"))
-                               
+        graph                   
               << ActivationLayer(ActivationLayerInfo(ActivationFunction::TANH))
               
               << OutputLayer(get_output_accessor(common_params)).set_name("out1");
