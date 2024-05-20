59b0c407d6622b1e6ce977d7770ecdad85ae714f
dondavan <yeezushugh@gmail.com>
2024-05-20 15:04:22 +0200

    fixing

--- utils/Power/Power.h
+++ utils/Power/Power.h
@@ -20,46 +20,48 @@ static int
 GPIOExport(int pin)
 {
 #define BUFFER_MAX 3
-	char buffer[BUFFER_MAX];
-	ssize_t bytes_written;
-	int fd;
+    char    buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int     fd;
 
-	fd = open("/sys/class/gpio/export", O_WRONLY);
-	if (-1 == fd) {
-		fprintf(stderr, "Failed to open export for writing!\n");
-		return(-1);
-	}
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if(-1 == fd)
+    {
+        fprintf(stderr, "Failed to open export for writing!\n");
+        return (-1);
+    }
 
-	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
-	write(fd, buffer, bytes_written);
-	close(fd);
-	return(0);
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
 }
-
 
 static int
 GPIODirection(int pin, int dir)
 {
-	static const char s_directions_str[]  = "in\0out";
+    static const char s_directions_str[] = "in\0out";
 
 #define DIRECTION_MAX 35
-	char path[DIRECTION_MAX];
-	int fd;
+    char path[DIRECTION_MAX];
+    int  fd;
 
-	snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
-	fd = open(path, O_WRONLY);
-	if (-1 == fd) {
-		fprintf(stderr, "Failed to open gpio direction for writing!\n");
-		return(-1);
-	}
+    snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+    fd = open(path, O_WRONLY);
+    if(-1 == fd)
+    {
+        fprintf(stderr, "Failed to open gpio direction for writing!\n");
+        return (-1);
+    }
 
-	if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
-		fprintf(stderr, "Failed to set direction!\n");
-		return(-1);
-	}
+    if(-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3))
+    {
+        fprintf(stderr, "Failed to set direction!\n");
+        return (-1);
+    }
 
-	close(fd);
-	return(0);
+    close(fd);
+    return (0);
 }
 
 static int
@@ -89,22 +91,22 @@ GPIOWrite(int pin, int value)
     return (0);
 }
 
-
 static int
 GPIOUnexport(int pin)
 {
-	char buffer[BUFFER_MAX];
-	ssize_t bytes_written;
-	int fd;
+    char    buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int     fd;
 
-	fd = open("/sys/class/gpio/unexport", O_WRONLY);
-	if (-1 == fd) {
-		fprintf(stderr, "Failed to open unexport for writing!\n");
-		return(-1);
-	}
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if(-1 == fd)
+    {
+        fprintf(stderr, "Failed to open unexport for writing!\n");
+        return (-1);
+    }
 
-	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
-	write(fd, buffer, bytes_written);
-	close(fd);
-	return(0);
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
 }
