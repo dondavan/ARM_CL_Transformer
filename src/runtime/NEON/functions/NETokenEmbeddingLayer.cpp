0d7d4100662a82f245ebd50278586dc93cfe143c
dondavan <yeezushugh@gmail.com>
2024-06-06 16:51:46 +0200

    timming

--- src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
+++ src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
@@ -25,12 +25,21 @@ NETokenEmbeddingLayer::~NETokenEmbeddingLayer() = default;
 
 void NETokenEmbeddingLayer::configure(ITensor *input, ITensor *vocab, ITensor *output, const EmbeddingLayerInfo& emb_info)
 {
+    #ifdef MEASURE_TIME
+    auto start_time = std::chrono::high_resolution_clock::now();
+    #endif
     _impl->src      = input;
     _impl->vocab    = vocab;
     _impl->dst      = output;
 
     _impl->op  = std::make_unique<cpu::CpuTokenEmbed>();
     _impl->op->configure(_impl->src->info(),_impl->vocab->info(), _impl->dst->info(), emb_info);
+
+    #ifdef MEASURE_TIME
+    auto end_time = std::chrono::high_resolution_clock::now();
+    double cost_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
+    std::cout << "TokenEmbeddingLayer::configure cost: " << cost_time << std::endl;
+    #endif
 }
 
 void NETokenEmbeddingLayer::prepare()
@@ -39,11 +48,19 @@ void NETokenEmbeddingLayer::prepare()
 
 void NETokenEmbeddingLayer::run()
 {
+    #ifdef MEASURE_TIME
+    auto start_time = std::chrono::high_resolution_clock::now();
+    #endif
     ITensorPack pack;
     pack.add_tensor(TensorType::ACL_SRC_0, _impl->src);
     pack.add_tensor(TensorType::ACL_SRC_1, _impl->vocab);
     pack.add_tensor(TensorType::ACL_DST, _impl->dst);
     _impl->op->run(pack);
+    #ifdef MEASURE_TIME
+    auto end_time = std::chrono::high_resolution_clock::now();
+    double cost_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
+    std::cout << "NETokenEmbeddingLayer::run cost: " << cost_time << std::endl;
+    #endif
 }
 
 } // namespace arm_compute
