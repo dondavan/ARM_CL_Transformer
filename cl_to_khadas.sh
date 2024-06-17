d143c0095829fe5b830a16c584377adfb910ef90
dondavan <yeezushugh@gmail.com>
2024-06-17 12:43:45 +0200

    CL

--- cl_to_khadas.sh
+++ cl_to_khadas.sh
@@ -1,2 +1,2 @@
-sshpass -p 'khadas' build/libarm_compute* Khadas.local:/home/khadas/run/lib 
-sshpass -p 'khadas' build/examples/graph_bert_base_uncased_CL Khadas.local:/home/khadas/run
+sshpass -p 'khadas' scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
+sshpass -p 'khadas' scp build/examples/graph_bert_base_uncased_CL Khadas.local:/home/khadas/run
