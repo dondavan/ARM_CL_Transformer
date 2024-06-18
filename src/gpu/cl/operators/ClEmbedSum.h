5ea23543f3ba80e20556200df5934d05a876fdda
dondavan <yeezushugh@gmail.com>
2024-06-18 11:10:03 +0200

    CL

--- src/gpu/cl/operators/ClEmbedSum.h
+++ src/gpu/cl/operators/ClEmbedSum.h
@@ -1,12 +1,13 @@
 #ifndef ARM_COMPUTE_CL_EMBED_SUM_H
 #define ARM_COMPUTE_CL_EMBED_SUM_H
 
+#include "src/gpu/cl/ClCompileContext.h"
 #include "src/gpu/cl/IClOperator.h"
+
 #include "arm_compute/core/TensorInfo.h"
 #include "arm_compute/core/Types.h"
 
-#include "src/cpu/kernels/CpuAddKernel.h"
-
+#include "src/gpu/cl/kernels/ClElementwiseKernel.h"
 
 namespace arm_compute
 {
@@ -15,19 +16,21 @@ namespace opencl
 /** A function use @ref kernels::CpuAddKernel to sum 3 embedding output*/
 class ClEmbedSum : public IClOperator
 {
-public:
+    public:
     /** Configure operator for a given list of arguments
      *
+     * @param[in]  compile_context The compile context to be used.
      * @param[in]  token        Token embedding input, Data type supported: F32
      * @param[in]  segemnt      Token embedding input, Data type supported: F32
      * @param[in]  position     Token embedding input, Data type supported: F32
      * @param[out] output       Destination tensor info. Data type supported: F32
      * @param[in]  emb_info     Embedding layer parameters.
      */
-    void configure(const ITensorInfo *token,
-                   const ITensorInfo *segemnt,
-                   const ITensorInfo *position,
-                   ITensorInfo *output, 
+    void configure(const ClCompileContext   &compile_context,
+                    ITensorInfo        *token,
+                    ITensorInfo        *segemnt,
+                    ITensorInfo        *position,
+                   ITensorInfo              *output,
                    const EmbeddingLayerInfo &emb_info);
     /** Static function to check if given info will lead to a valid configuration
      *
@@ -35,29 +38,30 @@ public:
      *
      * @return a status
      */
-    static Status validate(const ITensorInfo *token,
-                           const ITensorInfo *segemnt,
-                           const ITensorInfo *position,
-                           ITensorInfo *output, 
+    static Status validate(const ITensorInfo        *token,
+                           const ITensorInfo        *segemnt,
+                           const ITensorInfo        *position,
+                           ITensorInfo              *output,
                            const EmbeddingLayerInfo &emb_info);
 
     // Inherited methods overridden:
     void run(ITensorPack &tensors) override;
-private:
+
+    private:
     enum AuxTensorIdx
     {
         TokenSegmentOutput = 0,
         Count
     };
 
-    std::unique_ptr<kernels::CpuAddKernel> _add_kernel_1{nullptr};
-    std::unique_ptr<kernels::CpuAddKernel> _add_kernel_2{nullptr};
+    std::unique_ptr<kernels::ClSaturatedArithmeticKernel> _add_kernel_1{ nullptr };
+    std::unique_ptr<kernels::ClSaturatedArithmeticKernel> _add_kernel_2{ nullptr };
 
     TensorInfo _tmp_token_segment{};
 
-    experimental::MemoryRequirements _aux_mem{Count};
+    experimental::MemoryRequirements _aux_mem{ Count };
 
-    size_t _split_dimension{Window::DimY};
+    size_t _split_dimension{ Window::DimY };
 };
 } // namespace opencl
 } // namespace arm_compute
