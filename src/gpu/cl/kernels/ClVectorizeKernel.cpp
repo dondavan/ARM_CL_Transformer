05cca62f0bf54aa2e10e8195dfff997f0b181326
dondavan <yeezushugh@gmail.com>
2024-06-18 11:43:51 +0200

    CL

--- src/gpu/cl/kernels/ClVectorizeKernel.cpp
+++ src/gpu/cl/kernels/ClVectorizeKernel.cpp
@@ -1,6 +1,6 @@
 #include "src/gpu/cl/kernels/ClVectorizeKernel.h"
 
-#include "arm_compute/core/ITensor.h"
+#include "arm_compute/core/CL/ICLTensor.h"
 #include "arm_compute/core/TensorInfo.h"
 #include "arm_compute/core/Utils.h"
 
@@ -9,6 +9,8 @@
 #include "src/core/helpers/AutoConfiguration.h"
 #include "src/core/helpers/WindowHelpers.h"
 #include "src/cpu/kernels/vectorize/list.h"
+
+#include "support/Cast.h"
 
 namespace arm_compute
 {
@@ -19,30 +21,21 @@ namespace kernels
 
 namespace
 {
+/*
 static const std::vector<ClVectorizeKernel::VectorizeKernel> available_kernels = {
-    /*
-#ifdef ARM_COMPUTE_ENABLE_SVE
-    // TBA
-#endif // ARM_COMPUTE_ENABLE_SVE
-
-#ifdef __aarch64__
-    // TBA
-#endif // __aarch64__
-*/
     { "neon_vectorize_int_2_float32", [](const VectorizeKernelDataTypeISASelectorData &data)
       { return data.dt == DataType::F32; },
       REGISTER_FP32_NEON(arm_compute::cpu::neon_vectorize_int_2_float32) },
 
-};
+};*/
 } // namespace
 
-void ClVectorizeKernel::configure(const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst)
+void ClVectorizeKernel::configure(const CLCompileContext &compile_context, const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst)
 {
     ARM_COMPUTE_ERROR_ON_NULLPTR(src);
     ARM_COMPUTE_ERROR_ON_NULLPTR(vector);
 
-    const auto uk = ClVectorizeKernel::get_implementation(
-        VectorizeKernelDataTypeISASelectorData{ dst->data_type(), CPUInfo::get().get_isa() });
+    //const auto uk = ClVectorizeKernel::get_implementation( VectorizeKernelDataTypeISASelectorData{ dst->data_type(), CPUInfo::get().get_isa() });
 
     // Configure output tensor info.
     const TensorShape dst_shape(vector->tensor_shape().x(), src->tensor_shape().x());
@@ -57,13 +50,13 @@ void ClVectorizeKernel::configure(const ITensorInfo *src, const ITensorInfo *vec
 
     ARM_COMPUTE_ERROR_ON_NULLPTR(uk);
 
-    _run_method = uk->ukernel;
-    _name       = std::string("ClVectorizeKernel").append("/").append(uk->name);
+    CLBuildOptions build_opts;
+    build_opts.add_option("-DDATA_TYPE_IN_BYTES=" + support::cpp11::to_string(src->element_size()));
 
-    Window win;
+    _kernel = create_kernel(compile_context, "transpose", build_opts.options());
 
-    win = calculate_max_window(*src, Steps());
-    ICPPKernel::configure(win);
+    Window win = calculate_max_window(*src, Steps());
+    ICLKernel::configure_internal(win);
 }
 
 Status ClVectorizeKernel::validate(const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst)
@@ -74,27 +67,26 @@ Status ClVectorizeKernel::validate(const ITensorInfo *src, const ITensorInfo *ve
     return Status{};
 }
 
-
-void ClVectorizeKernel::run_op(ITensorPack &tensors, const Window &window, const ThreadInfo &info)
+void ClVectorizeKernel::run_op(ITensorPack &tensors, const Window &window, cl::CommandQueue &queue)
 {
-    ARM_COMPUTE_UNUSED(info);
     ARM_COMPUTE_ERROR_ON_UNCONFIGURED_KERNEL(this);
     ARM_COMPUTE_ERROR_ON_INVALID_SUBWINDOW(IKernel::window(), window);
 
     ARM_COMPUTE_ERROR_ON(tensors.empty());
     ARM_COMPUTE_ERROR_ON(_run_method == nullptr);
 
-    const ITensor *src    = tensors.get_const_tensor(TensorType::ACL_SRC_0);
-    const ITensor *vector = tensors.get_const_tensor(TensorType::ACL_SRC_1);
-    ITensor       *dst    = tensors.get_tensor(TensorType::ACL_DST);
+    const auto src =
+        utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_0));
+    const auto vector =
+        utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_1));
+    auto dst = utils::cast::polymorphic_downcast<ICLTensor *>(tensors.get_tensor(TensorType::ACL_DST));
 
-    _run_method(src, vector, dst, window);
-}
+    Window slice = window.first_slice_window_3D();
 
-
-const std::vector<ClVectorizeKernel::VectorizeKernel> &ClVectorizeKernel::get_available_kernels()
-{
-    return available_kernels;
+    unsigned int idx = 0;
+    add_3D_tensor_argument(idx, src, window);
+    add_3D_tensor_argument(idx, dst, window);
+    enqueue(queue, *this, slice, lws_hint());
 }
 
 } // namespace kernels
--- src/gpu/cl/kernels/ClVectorizeKernel.h
+++ src/gpu/cl/kernels/ClVectorizeKernel.h
@@ -1,8 +1,9 @@
 #ifndef SRC_CPU_KERNELS_CL_VECTORIZE_KERNEL_H
 #define SRC_CPU_KERNELS_CL_VECTORIZE_KERNEL_H
 
-
 #include "src/core/common/Macros.h"
+
+#include "src/gpu/cl/ClCompileContext.h"
 #include "src/gpu/cl/IClKernel.h"
 
 namespace arm_compute
@@ -14,10 +15,11 @@ namespace kernels
 /** Interface for the vectorization kernel */
 class ClVectorizeKernel : public IClKernel
 {
-private:
+    private:
     using VectorizeKernelPtr =
         std::add_pointer<void(const ITensor *, const ITensor *, ITensor *, const Window &)>::type;
-public:
+
+    public:
     /* Default Constructor */
     ClVectorizeKernel() = default;
     ARM_COMPUTE_DISALLOW_COPY_ALLOW_MOVE(ClVectorizeKernel);
@@ -28,7 +30,7 @@ public:
      * @param[out]  dst             Destination tensor info. Data type supported: F32
      * @param[in]   tkemb_info      Token embedding layer information.
      */
-    void configure(const ITensorInfo *src, const ITensorInfo *vector,  ITensorInfo *dst);
+    void configure(const CLCompileContext &compile_context, const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst);
     /** Static function to check if given info will lead to a valid configuration
      *
      * Similar to ClVectorizeKernel::configure()
@@ -38,35 +40,15 @@ public:
     static Status validate(const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst);
 
     // Inherited methods overridden:
-    void        run_op(ITensorPack &tensors, const Window &window, const ThreadInfo &info) override;
+    //void        run_op(ITensorPack &tensors, const Window &window, const ThreadInfo &info) override;
 
-    /** Get the preferred dimension in which the scheduler splits the work into multiple jobs.
-     *
-     * @return The split dimension hint.
-     */
-    size_t get_split_dimension_hint() const
-    {
-        return _split_dimension;
-    }
+    // Inherited methods overridden:
+    void run_op(ITensorPack &tensors, const Window &window, cl::CommandQueue &queue) override;
 
-    struct VectorizeKernel
-    {
-        const char                                         *name;
-        const VectorizeKernelDataTypeISASelectorDataPtr    is_selected;
-        VectorizeKernelPtr                                 ukernel;
-    };
-
-    static const std::vector<VectorizeKernel> &get_available_kernels();
-
-    
-private:
-    VectorizeKernelPtr      _run_method{nullptr};
-    size_t                  _split_dimension{Window::DimY};
-    std::string             _name{};
 };
 
-} // kernels
-} // namespace cpu
+} // namespace kernels
+} // namespace opencl
 } // namespace arm_compute
 
 #endif /* SRC_CPU_KERNELS_CL_VECTORIZE_KERNEL_H */
