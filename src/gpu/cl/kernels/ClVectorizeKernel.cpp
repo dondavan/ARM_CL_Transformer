6ed2c290d466f42f371c97ff9aa18898b4dfbe72
dondavan <yeezushugh@gmail.com>
2024-06-18 18:20:33 +0200

    CL

--- src/gpu/cl/kernels/ClVectorizeKernel.cpp
+++ src/gpu/cl/kernels/ClVectorizeKernel.cpp
@@ -21,13 +21,48 @@ namespace kernels
 
 namespace
 {
-/*
-static const std::vector<ClVectorizeKernel::VectorizeKernel> available_kernels = {
-    { "neon_vectorize_int_2_float32", [](const VectorizeKernelDataTypeISASelectorData &data)
-      { return data.dt == DataType::F32; },
-      REGISTER_FP32_NEON(arm_compute::cpu::neon_vectorize_int_2_float32) },
 
-};*/
+
+/**  Vectorize pretrained position embedding*/
+template <typename T>
+void run_vectorize(const Window &window, const ITensor *src, const ITensor *vector, ITensor *dst)
+{
+    /* Runtime reshape valid tensor region if input has been reshaped during preprocess */
+    size_t reshape_input_x = src->info()->valid_region().shape.x();
+    if(src->info()->tensor_shape().x() != reshape_input_x)
+    {
+        dst->info()->set_valid_region(dst->info()->valid_region().set(0,0,reshape_input_x));
+    }
+    Window win(window);
+
+    const unsigned int window_start_x   = static_cast<unsigned int>(win.x().start());
+    const unsigned int window_end_x     = static_cast<unsigned int>(win.x().end());
+
+    const unsigned int vector_depth     = vector->info()->tensor_shape().x(); 
+
+    unsigned int offset_vector,offset_dst;
+
+    win.set(Window::DimX, Window::Dimension(0,1,1));
+    Iterator src_iter(src,win);
+    Iterator dst_iter(dst,win);
+    Iterator vector_iter(vector,win);
+
+    const auto src_ptr      = reinterpret_cast<unsigned int *>(src_iter.ptr());
+    const auto dst_ptr      = reinterpret_cast<float *>(dst_iter.ptr());
+    const auto vector_ptr   = reinterpret_cast<float *>(vector_iter.ptr());
+    execute_window_loop(win,
+        [&](const Coordinates &)
+        {
+            for(unsigned int x = window_start_x; x < window_end_x; x++)
+            {
+                offset_dst     = x * vector_depth;
+                offset_vector  = *(src_ptr+x) * vector_depth;
+                std::memcpy(dst_ptr + offset_dst, vector_ptr + offset_vector, (vector_depth) * sizeof(*vector_ptr));
+            }
+            
+        }, src_iter);
+}
+
 } // namespace
 
 void ClVectorizeKernel::configure(const CLCompileContext &compile_context, const ITensorInfo *src, const ITensorInfo *vector, ITensorInfo *dst)
@@ -35,7 +70,6 @@ void ClVectorizeKernel::configure(const CLCompileContext &compile_context, const
     ARM_COMPUTE_ERROR_ON_NULLPTR(src);
     ARM_COMPUTE_ERROR_ON_NULLPTR(vector);
 
-    //const auto uk = ClVectorizeKernel::get_implementation( VectorizeKernelDataTypeISASelectorData{ dst->data_type(), CPUInfo::get().get_isa() });
 
     // Configure output tensor info.
     const TensorShape dst_shape(vector->tensor_shape().x(), src->tensor_shape().x());
@@ -48,13 +82,7 @@ void ClVectorizeKernel::configure(const CLCompileContext &compile_context, const
         dst->set_tensor_shape(dst_shape);
     }
 
-    ARM_COMPUTE_ERROR_ON_NULLPTR(uk);
-
-    CLBuildOptions build_opts;
-    build_opts.add_option("-DDATA_TYPE_IN_BYTES=" + support::cpp11::to_string(src->element_size()));
-
-    _kernel = create_kernel(compile_context, "transpose", build_opts.options());
-
+    // Configure kernel window
     Window win = calculate_max_window(*src, Steps());
     ICLKernel::configure_internal(win);
 }
@@ -71,21 +99,15 @@ void ClVectorizeKernel::run_op(ITensorPack &tensors, const Window &window, cl::C
 {
     ARM_COMPUTE_ERROR_ON_UNCONFIGURED_KERNEL(this);
     ARM_COMPUTE_ERROR_ON_INVALID_SUBWINDOW(IKernel::window(), window);
+    ARM_COMPUTE_UNUSED(queue);
 
     ARM_COMPUTE_ERROR_ON(tensors.empty());
-    ARM_COMPUTE_ERROR_ON(_run_method == nullptr);
-
     const auto src =
         utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_0));
-    //const auto vector = utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_1));
+    const auto vector = utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_1));
     auto dst = utils::cast::polymorphic_downcast<ICLTensor *>(tensors.get_tensor(TensorType::ACL_DST));
-
-    Window slice = window.first_slice_window_3D();
-
-    unsigned int idx = 0;
-    add_3D_tensor_argument(idx, src, window);
-    add_3D_tensor_argument(idx, dst, window);
-    enqueue(queue, *this, slice, lws_hint());
+    
+    run_vectorize<float>(window, src, vector, dst);
 }
 
 } // namespace kernels
