9543ff4d79c35704e5c3fbe762e614379c27c4f8
dondavan <yeezushugh@gmail.com>
2024-06-20 11:41:50 +0200

    CL

--- src/cpu/kernels/CpuVectorizeKernel.cpp
+++ src/cpu/kernels/CpuVectorizeKernel.cpp
@@ -62,6 +62,9 @@ void CpuVectorizeKernel::configure(const ITensorInfo *src, const ITensorInfo *ve
 
     Window win;
 
+    std::cout << "window x" << win.x().end() << std::endl;
+    std::cout << "window y" << win.y().end() << std::endl;
+    std::cout << "window z" << win.z().end() << std::endl;
     win = calculate_max_window(*src, Steps());
     ICPPKernel::configure(win);
 }
