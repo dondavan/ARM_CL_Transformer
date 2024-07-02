4aeeb8326893e5d026e284c1a8f1cb981efa7e62
dondavan <yeezushugh@gmail.com>
2024-07-02 19:44:36 +0200

    CL

--- examples/graph_bert_base_uncased_CL.cpp
+++ examples/graph_bert_base_uncased_CL.cpp
@@ -174,21 +174,15 @@ class GraphVanillaTransformerExample : public Example
                            unsigned int d_model, unsigned int h, float eps, unsigned int d_ff)
     {
         ARM_COMPUTE_UNUSED(h,eps,d_ff);
-        SubStream without_attention(graph);
-        SubStream with_attention(graph);
+        graph <<  MultiHeadLinearLayer(LinearLayerInfo(d_model), 
+                                    get_weights_accessor(data_path + layer_path, "query_weight.npy"),
+                                    get_weights_accessor(data_path + layer_path, "query_bias.npy"),
+                                    get_weights_accessor(data_path + layer_path, "key_weight.npy"),
+                                    get_weights_accessor(data_path + layer_path, "key_bias.npy"),
+                                    get_weights_accessor(data_path + layer_path, "value_weight.npy"),
+                                    get_weights_accessor(data_path + layer_path, "value_bias.npy"))
 
-        with_attention
-            /* Self Attention */
-            << MultiHeadLinearLayer(LinearLayerInfo(d_model), get_weights_accessor(data_path+layer_path, "query_weight.npy"),
-                                    get_weights_accessor(data_path+layer_path, "query_bias.npy"),
-                                    get_weights_accessor(data_path+layer_path, "key_weight.npy"),
-                                    get_weights_accessor(data_path+layer_path, "key_bias.npy"),
-                                    get_weights_accessor(data_path+layer_path, "value_weight.npy"),
-                                    get_weights_accessor(data_path+layer_path, "value_bias.npy"))
             << MultiHeadAttentionLayer(MultiHeadAttentionLayerInfo(d_model, h)).set_name("mha1");
-
-        graph << EltwiseLayer(std::move(with_attention), std::move(without_attention), EltwiseOperation::Add).set_name("add_4_norm_attention");
-
     }
 };
 
