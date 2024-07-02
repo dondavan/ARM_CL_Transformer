c481c75725557060d9b647903d444def5d8b2924
dondavan <yeezushugh@gmail.com>
2024-07-02 19:51:44 +0200

    CL

--- examples/graph_bert_base_uncased_CL.cpp
+++ examples/graph_bert_base_uncased_CL.cpp
@@ -180,9 +180,9 @@ class GraphVanillaTransformerExample : public Example
                                     get_weights_accessor(data_path + layer_path, "key_weight.npy"),
                                     get_weights_accessor(data_path + layer_path, "key_bias.npy"),
                                     get_weights_accessor(data_path + layer_path, "value_weight.npy"),
-                                    get_weights_accessor(data_path + layer_path, "value_bias.npy"));
+                                    get_weights_accessor(data_path + layer_path, "value_bias.npy"))
 
-            //<< MultiHeadAttentionLayer(MultiHeadAttentionLayerInfo(d_model, h)).set_name("mha1");
+            << MultiHeadAttentionLayer(MultiHeadAttentionLayerInfo(d_model, h)).set_name("mha1");
     }
 };
 
