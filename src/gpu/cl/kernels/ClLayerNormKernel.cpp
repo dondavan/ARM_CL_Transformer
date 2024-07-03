257b8bb056088b1d6b6ec1f2adb1c8cf88496b24
dondavan <yeezushugh@gmail.com>
2024-07-03 17:11:07 +0200

    CL

--- src/gpu/cl/kernels/ClLayerNormKernel.cpp
+++ src/gpu/cl/kernels/ClLayerNormKernel.cpp
@@ -23,67 +23,6 @@ namespace opencl
 namespace kernels
 {
 
-namespace
-{
-void layer_norm_fp32(const ITensor *src, ITensor *dst, const Window &window, float epsilon,
-                     float gamma,
-                     float beta,
-                     int   layer_axis)
-{
-    const int  window_step_axis  = 1;
-    const auto window_start_axis = static_cast<int>(window[layer_axis].start());
-    const auto window_end_axis   = static_cast<int>(window[layer_axis].end());
-
-    Window win = window; //.collapse_if_possible(window, Window::DimZ)
-    win.set(layer_axis, Window::Dimension(0, 1, 1));
-
-    Iterator input(src, win);
-    Iterator output(dst, win);
-
-    execute_window_loop(
-        win,
-        [&](const Coordinates &)
-        {
-            const auto input_ptr  = reinterpret_cast<const float *>(input.ptr());
-            const auto output_ptr = reinterpret_cast<float *>(output.ptr());
-            float      mean       = 0;
-            float      var        = 0;
-            float      res;
-
-            const int axis_len = window_end_axis - window_step_axis;
-            /* Calculate mean */
-            int axis = window_start_axis;
-            for(; axis <= axis_len; axis += window_step_axis)
-            {
-                mean += *(input_ptr + axis);
-            }
-            mean = mean / (axis_len + 1);
-
-            /* Calculate variance */
-            axis = window_start_axis;
-            for(; axis <= axis_len; axis += window_step_axis)
-            {
-                var += (*(input_ptr + axis) - mean) * (*(input_ptr + axis) - mean);
-            }
-            var = var / (axis_len + 1);
-
-            /* Calculate layer normalization */
-            axis = window_start_axis;
-            for(; axis <= axis_len; axis += window_step_axis)
-            {
-                res                                           = ((*(input_ptr + axis) - mean) / sqrt(var + epsilon)) * gamma + beta;
-                *reinterpret_cast<float *>(output_ptr + axis) = res;
-            }
-
-            ARM_COMPUTE_UNUSED(epsilon);
-            ARM_COMPUTE_UNUSED(input_ptr);
-            ARM_COMPUTE_UNUSED(output_ptr);
-        },
-        input, output);
-}
-
-} // namespace
-
 void ClLayerNormKernel::configure(const ClCompileContext &compile_context,
                                   const ITensorInfo            *input,
                                   ITensorInfo            *output,
