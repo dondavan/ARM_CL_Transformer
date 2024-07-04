4b4b7d7acbc672c5512b4d46d76f55a733953358
dondavan <yeezushugh@gmail.com>
2024-07-04 12:37:43 +0200

    CL

--- src/gpu/cl/kernels/ClLayerNormKernel.cpp
+++ src/gpu/cl/kernels/ClLayerNormKernel.cpp
@@ -89,6 +89,7 @@ void ClLayerNormKernel::run_op(ITensorPack &tensors, const Window &window, cl::C
     Window window_in{window};
     window_in.set(Window::DimX,
                               Window::Dimension(0, _input->dimension(0), _input->dimension(0)));
+    window_in.set_broadcasted(Window::DimZ);
 
     Window slice = window_in.first_slice_window_3D();
 
