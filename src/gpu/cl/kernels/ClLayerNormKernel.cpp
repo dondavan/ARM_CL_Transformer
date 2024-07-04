f5844a6535ce86c6a6e80ad552dccb243d19db2e
dondavan <yeezushugh@gmail.com>
2024-07-04 17:30:01 +0200

    CL

--- src/gpu/cl/kernels/ClLayerNormKernel.cpp
+++ src/gpu/cl/kernels/ClLayerNormKernel.cpp
@@ -90,8 +90,6 @@ void ClLayerNormKernel::run_op(ITensorPack &tensors, const Window &window, cl::C
     window_in.set(Window::DimX,Window::Dimension(0, _input->dimension(0), _input->dimension(0)));
 
     Window slice = window_in.first_slice_window_3D();
-    slice.set_broadcasted(Window::DimZ);
-
     unsigned int idx = 0;
     add_3D_tensor_argument(idx, input, slice);
     add_3D_tensor_argument(idx, output, slice);
