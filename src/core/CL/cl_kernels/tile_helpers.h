bf50e9fb2ba1856c324bc1b3f2e27200da77745d
dondavan <yeezushugh@gmail.com>
2024-07-07 16:47:06 +0200

    CL

--- src/core/CL/cl_kernels/tile_helpers.h
+++ src/core/CL/cl_kernels/tile_helpers.h
@@ -76,7 +76,7 @@
     union {                                 \
         DATA_TYPE                      s[TILE_VECTOR_SIZE##W];                  \
         TILE_VECTOR_TYPE##W(DATA_TYPE) v;                     \
-    } BASENAME[H]
+    } __attribute__((aligned(32))) BASENAME[H]
 
 // Hugh added local
 
