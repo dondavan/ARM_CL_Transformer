228e48b4e655ebad66dccef849cff40d65470071
dondavan <yeezushugh@gmail.com>
2024-07-08 17:29:15 +0200

    CL

--- src/core/CL/cl_kernels/tile_helpers.h
+++ src/core/CL/cl_kernels/tile_helpers.h
@@ -76,7 +76,7 @@
     struct {                                 \
         DATA_TYPE                      s[TILE_VECTOR_SIZE##W];                  \
         TILE_VECTOR_TYPE##W(DATA_TYPE) v;                     \
-    } BASENAME[H]
+    } __local BASENAME[H]
 
 // Hugh changed union to struct
 
