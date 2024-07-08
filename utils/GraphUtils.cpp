58956b5d2b9d6ec2342c6949e283dc7ac89cdc81
dondavan <yeezushugh@gmail.com>
2024-07-08 20:12:02 +0200

    CL

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -854,7 +854,7 @@ void RawResultAccessor::access_typed_tensor(ITensor &tensor)
     for(size_t y = 0; y < tensor.info()->tensor_shape().y(); y++)
     {
         _output_stream << " ################# " << y << " ################# " << std::endl;
-        for(size_t x = 0; x < tensor.info()->tensor_shape().x()-4; x+=4)
+        for(size_t x = 0; x <= tensor.info()->tensor_shape().x()-4; x+=4)
         {
             _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x,y)))  << ", ";
             _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x+1,y)))  << ", ";
