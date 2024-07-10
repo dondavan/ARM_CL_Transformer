a1be538a75c9fd6f376e4e8cfd00921763cbff68
dondavan <yeezushugh@gmail.com>
2024-07-10 19:01:17 +0200

    CL

--- src/core/CL/cl_kernels/tile_helpers.h
+++ src/core/CL/cl_kernels/tile_helpers.h
@@ -335,7 +335,7 @@
 #endif // !defined(UNROLL_WITH_PRAGMA)
 #define LOOP_UNROLLING(type, idx, start, step, num, macro) LOOP_UNROLLING_STR(type, idx, start, step, num, macro)
 
-#define LOOP_UNROLLING_HUGH_STR(type, idx, start, step, num, macro) {idx = start;LOOP_UNROLLING_##num(idx, step, macro);}
+#define LOOP_UNROLLING_HUGH_STR(type, idx, start, step, num, macro) idx = start;LOOP_UNROLLING_##num(idx, step, macro);
 #define LOOP_UNROLLING_HUGH(type, idx, start, step, num, macro) LOOP_UNROLLING_HUGH_STR(type, idx, start, step, num, macro)
 
 /** Get the get_global_id with partial N0. This function is useful when the dimension is not multiple of N0 and we need to use a partial N0
