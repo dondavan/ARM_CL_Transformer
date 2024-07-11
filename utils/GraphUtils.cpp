e6f2aa26d41b9d5abe9dc21762d04161524b281c
dondavan <yeezushugh@gmail.com>
2024-07-11 18:45:51 +0200

    CL

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -851,6 +851,7 @@ void RawResultAccessor::access_typed_tensor(ITensor &tensor)
 {
     _output_stream << std::scientific;
     _output_stream << "---------- Result ----------" << std::endl;
+    /*
     for(size_t y = 0; y < tensor.info()->tensor_shape().y(); y++)
     {
         _output_stream << " ################# " << y << " ################# " << std::endl;
@@ -865,29 +866,32 @@ void RawResultAccessor::access_typed_tensor(ITensor &tensor)
         _output_stream << " ************************************************ " << std::endl;
         _output_stream << " ************************************************ " << std::endl;
         _output_stream << " ************************************************ " << std::endl;
-    }
+    }*/
 
     _output_stream << "---------- Result ----------" << std::endl;
-    for(size_t y = 0; y < tensor.info()->tensor_shape().y(); y++)
+    for(size_t z = 0; z < tensor.info()->tensor_shape().z(); z++)
     {
-        size_t x = tensor.info()->tensor_shape().x();
-        _output_stream << "[ ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(0,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(1,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(2,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(3,y)))  << ", ";
+        for(size_t y = 0; y < tensor.info()->tensor_shape().y(); y++)
+        {
+            size_t x = tensor.info()->tensor_shape().x();
+            _output_stream << "[ ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(0,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(1,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(2,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(3,y,z)))  << ", ";
 
-        _output_stream  << " ..., ";
+            _output_stream  << " ..., ";
 
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-4,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-3,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-2,y)))  << ", ";
-        _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-1,y)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-4,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-3,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-2,y,z)))  << ", ";
+            _output_stream << *reinterpret_cast<T *>(tensor.ptr_to_element(Coordinates(x-1,y,z)))  << ", ";
 
-        _output_stream << "] ";
-        _output_stream << std::endl;
+            _output_stream << "] ";
+            _output_stream << std::endl;
+        }
+        _output_stream  << std::endl;
     }
-    _output_stream  << std::endl;
 }
 
 bool RawResultAccessor::access_tensor(ITensor &tensor)
