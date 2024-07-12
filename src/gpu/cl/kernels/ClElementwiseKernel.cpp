e64e9eaf1314479421b619189a3f1943cd67c698
dondavan <yeezushugh@gmail.com>
2024-07-12 13:36:46 +0200

    CL

--- src/gpu/cl/kernels/ClElementwiseKernel.cpp
+++ src/gpu/cl/kernels/ClElementwiseKernel.cpp
@@ -327,9 +327,6 @@ void ClElementwiseKernel::run_op(ITensorPack &tensors, const Window &window, ::c
     ARM_COMPUTE_ERROR_ON_UNCONFIGURED_KERNEL(this);
     ARM_COMPUTE_ERROR_ON_INVALID_SUBWINDOW(ICLKernel::window(), window);
     std::cout << "src/gpu/cl/kernels/ClElementwiseKernel.cpp ClElementwiseKernel::run_op start" << std::endl;
-    std::cout << "window.x().end() " <<  window.x().end() << std::endl;
-    std::cout << "window.y().end() " <<  window.y().end() << std::endl;
-    std::cout << "window.z().end() " <<  window.z().end() << std::endl;
 
     const auto src_0 =
         utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC_0));
@@ -365,6 +362,35 @@ void ClElementwiseKernel::run_op(ITensorPack &tensors, const Window &window, ::c
     Window slice_src1 = slice.broadcast_if_dimension_le_one(in_shape1_collapsed);
     Window slice_src2 = slice.broadcast_if_dimension_le_one(in_shape2_collapsed);
 
+    
+    std::cout << "src1 x" << src_0->info()->tensor_shape().x() << std::endl;
+    std::cout << "src1 y" << src_0->info()->tensor_shape().y() << std::endl;
+    std::cout << "src1 z" << src_0->info()->tensor_shape().z() << std::endl;
+
+    std::cout << "src2 x" << src_1->info()->tensor_shape().x() << std::endl;
+    std::cout << "src2 y" << src_1->info()->tensor_shape().y() << std::endl;
+    std::cout << "src2 z" << src_1->info()->tensor_shape().z() << std::endl;
+
+    std::cout << "dst x" << dst->info()->tensor_shape().x() << std::endl;
+    std::cout << "dst y" << dst->info()->tensor_shape().y() << std::endl;
+    std::cout << "dst z" << dst->info()->tensor_shape().z() << std::endl;
+
+    std::cout << "slice x" << slice.x().end() << std::endl;
+    std::cout << "slice y" << slice.y().end() << std::endl;
+    std::cout << "slice z" << slice.z().end() << std::endl;
+
+    std::cout << "slice_src1 x" << slice_src1.x().end() << std::endl;
+    std::cout << "slice_src1 y" << slice_src1.y().end() << std::endl;
+    std::cout << "slice_src1 z" << slice_src1.z().end() << std::endl;
+
+    std::cout << "slice_src2 x" << slice_src2.x().end() << std::endl;
+    std::cout << "slice_src2 y" << slice_src2.y().end() << std::endl;
+    std::cout << "slice_src2 z" << slice_src2.z().end() << std::endl;
+
+    std::cout << "lws x " << lws_hint().get()[0] << std::endl;
+    std::cout << "lws y " << lws_hint().get()[1] << std::endl;
+    std::cout << "lws z " << lws_hint().get()[2] << std::endl;
+    
 
     // Check whether it is in_place calculation
     const bool in_place = (src_0 == dst) || (src_1 == dst);
@@ -378,8 +404,7 @@ void ClElementwiseKernel::run_op(ITensorPack &tensors, const Window &window, ::c
             add_3D_tensor_argument(idx, dst, slice);
         }
 
-        //enqueue(queue, *this, slice, lws_hint());
-        enqueue(queue, *this, slice);
+        enqueue(queue, *this, slice, lws_hint());
         ARM_COMPUTE_UNUSED(collapsed.slide_window_slice_3D(slice_src1));
         ARM_COMPUTE_UNUSED(collapsed.slide_window_slice_3D(slice_src2));
     } while (collapsed.slide_window_slice_3D(slice));
