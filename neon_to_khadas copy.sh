8eec9e482d8a534340f6cf06bc62a830cb848499
dondavan <yeezushugh@gmail.com>
2024-07-14 19:43:27 +0200

    CL

--- neon_to_khadas copy.sh
+++
@@ -1,2 +0,0 @@
-sshpass -p 'khadas' scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
-sshpass -p 'khadas' scp build/examples/graph_bert_base_uncased Khadas.local:/home/khadas/run
--- neon_to_khadas.sh
+++ neon_to_khadas.sh
@@ -1,2 +1,3 @@
 sshpass -p 'khadas' scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
 sshpass -p 'khadas' scp build/examples/graph_bert_base_uncased Khadas.local:/home/khadas/run
+sshpass -p 'khadas' scp build/examples/graph_bert_large_uncased Khadas.local:/home/khadas/run
