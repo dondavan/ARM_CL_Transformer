920972f3d837229eb57191ef27eaca9b9203b6bb
dondavan <yeezushugh@gmail.com>
2024-07-14 21:01:14 +0200

    CL

--- build_cp_to_khadas.sh
+++ build_cp_to_khadas.sh
@@ -1,3 +1,5 @@
+git pull
+scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 examples=1 os=linux arch=armv8a measure=1 -j4
 sshpass -p 'khadas' scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
 sshpass -p 'khadas' scp build/examples/graph_bert_base_uncased_CL Khadas.local:/home/khadas/run
 sshpass -p 'khadas' scp build/examples/graph_bert_large_uncased_CL Khadas.local:/home/khadas/run
