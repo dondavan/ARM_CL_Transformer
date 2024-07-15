d3f2520af2454c13c0bade0c9d0b96228e57c15b
dondavan <yeezushugh@gmail.com>
2024-07-15 23:44:18 +0200

    CL

--- src/runtime/CL/functions/CLActivationLayer.cpp
+++ src/runtime/CL/functions/CLActivationLayer.cpp
@@ -32,6 +32,11 @@
 #include "src/core/CL/ICLKernel.h"
 #include "src/gpu/cl/operators/ClActivation.h"
 
+#ifdef MEASURE_TIME
+#include <chrono>
+#include <fstream>
+#endif
+
 namespace arm_compute
 {
 struct CLActivationLayer::Impl
@@ -70,6 +75,7 @@ void CLActivationLayer::configure(const CLCompileContext &compile_context,
 
     _impl->op = std::make_unique<opencl::ClActivation>();
     _impl->op->configure(compile_context, _impl->src->info(), _impl->dst->info(), act_info);
+
 #ifdef MEASURE_TIME
     auto   end_time  = std::chrono::high_resolution_clock::now();
     double cost_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
@@ -95,13 +101,13 @@ void CLActivationLayer::run()
     pack.add_tensor(TensorType::ACL_SRC, _impl->src);
     pack.add_tensor(TensorType::ACL_DST, _impl->dst);
     _impl->op->run(pack);
-    
+
 #ifdef MEASURE_TIME
     auto   end_time  = std::chrono::high_resolution_clock::now();
     double cost_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
     std::ofstream measure_out("measure_output.txt",std::ios::app);
     measure_out.precision(5);
-    measure_out << std::scientific << "NEActivationLayer::run cost: " << cost_time << std::endl;
+    measure_out << std::scientific << "CLActivationLayer::run cost: " << cost_time << std::endl;
     measure_out.close();
 #endif
 }
