a2bdb14ca3848afa04cda0cce32a5e5858f2df1e
dondavan <yeezushugh@gmail.com>
2024-07-22 00:42:43 +0200

    vector

--- examples/graph_bert_base_uncased_CL.cpp
+++ examples/graph_bert_base_uncased_CL.cpp
@@ -69,9 +69,9 @@ class GraphVanillaTransformerExample : public Example
         constexpr unsigned int d_vocab    = 30522U; // Vocaboary size
         constexpr unsigned int d_segemnt  = 2U;     // Sentence segmentation size
         constexpr unsigned int d_position = 512U;   // Pretrained positional encoding length
-        constexpr unsigned int h          = 12U;    // Parallel attention (Heads)
-        constexpr float        eps        = 1e-12;  // Layer normalization eplision
-        constexpr unsigned int d_ff       = 3072U;  // Dim feedforward
+        //constexpr unsigned int h          = 12U;    // Parallel attention (Heads)
+        //constexpr float        eps        = 1e-12;  // Layer normalization eplision
+        //constexpr unsigned int d_ff       = 3072U;  // Dim feedforward
         /*constexpr unsigned int d_q         = 64U;      // Dim query, 512U/8U
         constexpr unsigned int d_k           = 64U;      // Dim key, 512U/8U
         constexpr unsigned int d_v           = 64U;      // Dim value, 512U/8U
@@ -114,7 +114,7 @@ class GraphVanillaTransformerExample : public Example
                                 get_weights_accessor(data_path, "token_embedding.npy", operation_layout),
                                 get_weights_accessor(data_path, "segment_embedding.npy", operation_layout),
                                 get_weights_accessor(data_path, "positional_embedding.npy", operation_layout))
-                     .set_name("tkemb1");
+                     .set_name("tkemb1")
 
 
               << OutputLayer(get_output_accessor(common_params)).set_name("out1");
