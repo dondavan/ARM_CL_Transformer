d2225bd3dcbbd76ec5cfd071e9942365531b7c4c
dondavan <yeezushugh@gmail.com>
2024-08-03 16:59:35 +0200

    ff

--- examples/graph_bert_base_uncased_vector_960_ff_3584.cpp
+++ examples/graph_bert_base_uncased_vector_960_ff_3584.cpp
@@ -65,13 +65,13 @@ class GraphVanillaTransformerExample : public Example
         std::string data_path = common_params.data_path;
 
         // Model parameters
-        constexpr unsigned int d_model    = 384U;   // Dim layer output
+        constexpr unsigned int d_model    = 960U;   // Dim layer output
         constexpr unsigned int d_vocab    = 30522U; // Vocaboary size
         constexpr unsigned int d_segemnt  = 2U;     // Sentence segmentation size
         constexpr unsigned int d_position = 512U;   // Pretrained positional encoding length
         constexpr unsigned int h          = 12U;    // Parallel attention (Heads)
         constexpr float        eps        = 1e-12;  // Layer normalization eplision
-        constexpr unsigned int d_ff       = 1152U;  // Dim feedforward
+        constexpr unsigned int d_ff       = 3584U;  // Dim feedforward
         /*constexpr unsigned int d_q         = 64U;      // Dim query, 512U/8U
         constexpr unsigned int d_k           = 64U;      // Dim key, 512U/8U
         constexpr unsigned int d_v           = 64U;      // Dim value, 512U/8U
