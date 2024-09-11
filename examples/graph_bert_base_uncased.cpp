d8a7f98462141b0da34966c0eb003720164ae96a
dondavan <yeezushugh@gmail.com>
2024-09-11 15:17:07 +0200

    fixing

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -182,7 +182,6 @@ class GraphVanillaTransformerExample : public Example
     void add_encoder_block(std::string data_path, std::string layer_path,
                            unsigned int d_model, unsigned int h, float eps, unsigned int d_ff)
     {
-        ARM_COMPUTE_UNUSED(eps,d_ff);
         SubStream without_attention(graph);
         SubStream with_attention(graph);
 
@@ -197,6 +196,9 @@ class GraphVanillaTransformerExample : public Example
             << MultiHeadAttentionLayer(MultiHeadAttentionLayerInfo(d_model, h)).set_name("mha1");
 
         graph << EltwiseLayer(std::move(with_attention), std::move(without_attention), EltwiseOperation::Add).set_name("add_4_norm_attention");
+
+        /* Self output */
+        graph << LayerNormLayer(LayerNormLayerInfo(0 /*Window::DimX*/, eps));
 
     }
 };
