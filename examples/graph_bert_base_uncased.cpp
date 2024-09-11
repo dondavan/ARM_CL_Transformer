395b467c519942e02a1bfc0fb6693a70271d08d1
dondavan <yeezushugh@gmail.com>
2024-09-11 15:14:42 +0200

    fixing

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -182,6 +182,7 @@ class GraphVanillaTransformerExample : public Example
     void add_encoder_block(std::string data_path, std::string layer_path,
                            unsigned int d_model, unsigned int h, float eps, unsigned int d_ff)
     {
+        ARM_COMPUTE_UNUSED(eps,d_ff);
         SubStream without_attention(graph);
         SubStream with_attention(graph);
 
